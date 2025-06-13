fe6acf2a061c2c9d73ce884c82732755f33103a3
sakshii893 <sakshiiipawar10@gmail.com>
2025-06-13 21:36:44 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -22,9 +22,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/admin',adminRoutes)
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb://localhost:27017/mern-auth")
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    app.listen(5000, () => console.log(`Server running on port ${process.env.PORT}`));
   })
   .catch(err => console.log(err));
